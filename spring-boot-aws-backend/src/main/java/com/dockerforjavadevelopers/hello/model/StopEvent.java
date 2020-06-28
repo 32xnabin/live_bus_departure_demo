eb42781589dabd84af614eeff17ba5c245834937
Nabin <nabinwell@gmail.com>
2020-06-29 03:23:43 +0545

    backend

---
+++ spring-boot-aws-backend/src/main/java/com/dockerforjavadevelopers/hello/model/StopEvent.java
@@ -0,0 +1,51 @@
+package com.dockerforjavadevelopers.hello.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class StopEvent {
+
+    private String departureTimePlanned;
+
+    private String name;
+
+    private Transportation transportation;
+
+    public Transportation getTransportation() {
+        return transportation;
+    }
+
+    public String getDepartureTimePlanned() {
+
+        try {
+            // 2020-06-29T19:15:00Z
+
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+
+            Date date = inputFormat.parse(departureTimePlanned);
+
+            return date.getTime() + "";
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "Pending....";
+
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private Location location;
+
+    public Location getLocation() {
+        return location;
+    }
+
+}
